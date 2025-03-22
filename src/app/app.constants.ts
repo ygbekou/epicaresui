0b24085a8fac4138956d5380d63bac1fa78b3364
ygbekou <ericgbekou@hotmail.com>
2025-03-22 00:37:30 -0500

    Fixing stripe

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,8 +1,8 @@
 export class Constants {
-	//public static apiServer = 'http://localhost:8080';
+	public static apiServer = 'http://localhost:8080';
  	//public static apiServer = 'http://www.lamagottalent.com/wack';
 	// public static apiServer = 'http://www.bidamaservices.com/bsc';
-	public static apiServer = 'http://epicares.org:8080';
+	//public static apiServer = 'https://epicares.org:8443';
 	public static SUCCESS = 'success';
 	public static INFO = 'info';
 	public static WARN = 'warn';
